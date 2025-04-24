a7e2ea2133ca1d4d1063d4e1afb3573f3c5be0d7
Star <1647490509@qq.com>
2025-04-24 10:45:44 +0800

    Initial Commit

---
+++ 并发练习/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  并发练习
+//
+//  Created by Star. on 2025/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
